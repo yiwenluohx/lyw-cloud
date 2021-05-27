5d1606450a2f6911d3d9429079b2e7ac9b55655f
luohx <Lhongxiao@lcchain.com>
2021-05-27 14:59:36 +0800

    添加eureka集群

---
+++ cloud-eureka-server7002/src/main/java/com/study/cloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.study.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: luohx
+ * @Description: 描述
+ * @Date: 2021/5/27 0027 14:00
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
