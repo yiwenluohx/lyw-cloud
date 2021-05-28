7503f7c58606cdde9a2424ebecca289852f6d053
luohx <Lhongxiao@lcchain.com>
2021-05-28 10:55:54 +0800

    添加单节点的zk服务

---
+++ cloud-provider-payment8004/src/main/java/com/study/cloud/PaymentMain8004.java
@@ -0,0 +1,18 @@
+package com.study.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: luohx
+ * @Description: 描述
+ * @Date: 2021/5/28 0028 9:14
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
