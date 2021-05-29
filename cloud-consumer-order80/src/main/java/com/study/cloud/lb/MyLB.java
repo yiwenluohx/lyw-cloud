520423aa207422a3c0acaa0765eaefdcdd3e88b8
luohx <Lhongxiao@lcchain.com>
2021-05-30 07:41:34 +0800

    自定义负载均衡方法

--- cloud-consumer-order80/src/main/java/com/study/cloud/lb/MyLB.java
+++ cloud-consumer-order80/src/main/java/com/study/cloud/lb/MyLB.java
@@ -18,6 +18,18 @@ public class MyLB implements LoadBalancer {
 
     @Override
     public ServiceInstance instances(List<ServiceInstance> serviceInstances) {
-        return null;
+        int index = getAndIncrement() % serviceInstances.size();
+        return serviceInstances.get(index);
+    }
+
+    public final int getAndIncrement(){
+        int current;
+        int next;
+        do{
+           current = this.atomicInteger.get();
+           next = current >= 2147483647 ? 0 : current  + 1;
+        }while (!this.atomicInteger.compareAndSet(current, next));
+        System.out.println("....next:" + next);
+        return next;
     }
 }
