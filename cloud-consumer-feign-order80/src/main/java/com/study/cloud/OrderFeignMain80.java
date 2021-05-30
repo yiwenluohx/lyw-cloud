a783a1df450df166b62e90c182f7ddf461413516
luohx <Lhongxiao@lcchain.com>
2021-05-30 14:53:00 +0800

    新增openfeign服务调用

---
+++ cloud-consumer-feign-order80/src/main/java/com/study/cloud/OrderFeignMain80.java
@@ -0,0 +1,18 @@
+package com.study.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Author: luohx
+ * @Description:
+ * @Date: 2021/5/30 12:27
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class, args);
+    }
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/study/cloud/service/PaymentFeignService.java
@@ -0,0 +1,21 @@
+package com.study.cloud.service;
+
+import com.study.cloud.entities.CommonResult;
+import com.study.cloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @Author: luohx
+ * @Description:
+ * @Date: 2021/5/30 12:30
+ */
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+}
