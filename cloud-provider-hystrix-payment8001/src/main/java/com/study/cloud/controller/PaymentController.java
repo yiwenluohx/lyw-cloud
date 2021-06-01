c43dc1854be4e570e4ac141902fe423975d680ba
luohx <Lhongxiao@lcchain.com>
2021-06-01 11:40:36 +0800

    实现Hystrix服务熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/study/cloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/study/cloud/controller/PaymentController.java
@@ -32,9 +32,16 @@ public class PaymentController {
     }
 
     @GetMapping("/payment/hystrix/timeout/{id}")
-    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result = paymentService.paymentInfo_TimeOut(id);
         log.info("****result:" + result);
+        return result;
+    }
+
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("****result：" + result);
         return result;
     }
 
