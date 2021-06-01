28d524203e81835e26abfbe6f63d969808305a9f
luohx <Lhongxiao@lcchain.com>
2021-06-01 17:58:38 +0800

    新建config服务端

---
+++ cloud-config-center-3344/src/main/java/com/study/cloud/MainAppConfigCenter3344.java
@@ -0,0 +1,18 @@
+package com.study.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: luohx
+ * @Description: 描述
+ * @Date: 2021/6/1 0001 17:13
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class MainAppConfigCenter3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainAppConfigCenter3344.class, args);
+    }
+}
