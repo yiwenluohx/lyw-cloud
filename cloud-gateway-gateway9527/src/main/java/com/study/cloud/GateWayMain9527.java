14bee5f83452dda23b246af2cef89ebe1b2b90a1
luohx <Lhongxiao@lcchain.com>
2021-06-01 14:11:27 +0800

    yml文件配置gateway路由

---
+++ cloud-gateway-gateway9527/src/main/java/com/study/cloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.study.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: luohx
+ * @Description: 描述
+ * @Date: 2021/6/1 0001 11:50
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
