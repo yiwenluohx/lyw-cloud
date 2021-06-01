cd3284315ce15d530e527ec3acba92e235afacd1
luohx <Lhongxiao@lcchain.com>
2021-06-01 15:57:12 +0800

    gateway学习完毕

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @Author: luohx
+ * @Description: 描述
+ * @Date: 2021/6/1 0001 15:15
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+}
