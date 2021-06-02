58f9d59a85d663e15e7088a9eeac044c6ac8e456
luohx <Lhongxiao@lcchain.com>
2021-06-02 10:25:45 +0800

    添加config配置客户端

---
+++ cloud-config-client-3355/src/main/java/com/study/cloud/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.study.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: luohx
+ * @Description: 描述
+ * @Date: 2021/6/2 0002 9:51
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
