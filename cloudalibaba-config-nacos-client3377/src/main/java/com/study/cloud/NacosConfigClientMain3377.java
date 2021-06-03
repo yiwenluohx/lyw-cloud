f5fc2fe9b7fccd5478254076c6bfa04bc32e9696
luohx <Lhongxiao@lcchain.com>
2021-06-03 15:50:59 +0800

    nacos配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/study/cloud/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.study.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: luohx
+ * @Description: 描述
+ * @Date: 2021/6/3 0003 13:46
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
